9b9fc123f0a5ddf065fd191e4c237140153e4cb2
unknown <alesfluiz@gmail.com>
2023-05-27 19:58:17 -0300

    first commit

---
+++ LinqAndLambda/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndLambda.Entities
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+    }
+}
